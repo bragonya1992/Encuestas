0be0a1bda792e0c32df3d18316af71a7b5907623
bragonya1992 <brayan.yaquian@fox.com>
2018-05-26 21:58:21 -0600

    Run local survey

--- app/src/main/java/com/apps/brayan/surveyapp/SurveyScreen.kt
+++ app/src/main/java/com/apps/brayan/surveyapp/SurveyScreen.kt
@@ -13,9 +13,7 @@ import kotlinx.android.synthetic.main.activity_survey_screen.*
 
 
 class SurveyScreen : AppCompatActivity() {
-    companion object {
-        val domainName:String = "http://192.168.100.44:8080"
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.requestFeature(Window.FEATURE_PROGRESS)
@@ -28,24 +26,20 @@ class SurveyScreen : AppCompatActivity() {
         webview.getSettings().setJavaScriptEnabled(true)
 
         val activity = this
-        webview.setWebChromeClient(object : WebChromeClient() {
+        with(webview) {
+            setWebChromeClient(object : WebChromeClient() {
             override fun onProgressChanged(view: WebView, progress: Int) {
                 activity.setProgress(progress * 1000)
             }
         })
-        webview.setWebViewClient(object : WebViewClient() {
+            setWebViewClient(object : WebViewClient() {
             override fun onReceivedError(view: WebView, errorCode: Int, description: String, failingUrl: String) {
                 Toast.makeText(activity, "Oh no! $description", Toast.LENGTH_SHORT).show()
             }
         })
 
-        webview.loadUrl(domainName,getHeader())
-    }
-
-    fun getHeader(): HashMap<String,String>{
-        var hashMap:HashMap<String,String> = HashMap()
-        hashMap["json"] = "{\"pages\":[{\"name\":\"page1\",\"elements\":[{\"type\":\"radiogroup\",\"name\":\"question1\",\"choices\":[\"item1\",\"item2\",\"item3\"]},{\"type\":\"radiogroup\",\"name\":\"question3\",\"choices\":[\"item1\",\"item2\",\"item3\"]},{\"type\":\"checkbox\",\"name\":\"question2\",\"choices\":[\"item1\",\"item2\",\"item3\"]}]}]}"
-        return hashMap
+            loadUrl("file:///android_asset/surveycore/main.html")
+        }
     }
 
 }
